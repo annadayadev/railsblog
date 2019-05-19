3c2c4c5d0f20e4361b950126c02c41f955723873
annadayadev <ryry11adaya@gmail.com>
2019-05-19 19:31:26 +0800

    Added Bootstrap to blog

---
+++ app/models/article.rb
@@ -0,0 +1,7 @@
+class Article < ApplicationRecord
+
+    validates :title, presence: true, length: { minumum: 3, maximum: 50 }
+    validates :description, presence: true, length: { minumum: 3, maximum: 300 }
+
+end
+  
---
+++ db/migrate/20190519023810_add_author_to_articles.rb
@@ -0,0 +1,6 @@
+class AddAuthorToArticles < ActiveRecord::Migration[5.2]
+  def change
+    add_column :articles, :author, :string
+    
+  end
+end
